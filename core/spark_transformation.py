6320de1de8270485c2e4b4ce6632350f33029681
DamilolaAdeniji <dammy_adeniji@yahoo.com>
2025-02-10 22:13:16 +0100

    fixed linting issues

--- core/spark_transformation.py
+++ core/spark_transformation.py
@@ -7,6 +7,7 @@ data = spark.read.parquet("s3://final-aws-project/raw/311-calls-data.parquet")
 
 # TODO : filter the data to only include the following columns
 
+
 def spark_transform(data):
     df_filtered = data.select(
         "address",
@@ -37,17 +38,22 @@ def spark_transform(data):
     )
 
     df_filtered = (
-        df_filtered.withColumn("closed_date", to_timestamp(col("closed_date"), "MM/dd/yyyy hh:mm:ss a"))
-        .withColumn("data_as_of", to_timestamp(col("data_as_of"), "MM/dd/yyyy hh:mm:ss a"))
-        .withColumn("data_loaded_at", to_timestamp(col("data_loaded_at"), "MM/dd/yyyy hh:mm:ss a"))
-        .withColumn("requested_datetime", to_timestamp(col("requested_datetime"), "MM/dd/yyyy hh:mm:ss a"))
-        .withColumn("updated_datetime", to_timestamp(col("updated_datetime"), "MM/dd/yyyy hh:mm:ss a"))
-        .withColumn("lat", col("lat").cast("double"))
-        .withColumn("long", col("long").cast("double"))
-    )
+        df_filtered.withColumn("closed_date", to_timestamp(col("closed_date"), # noqa
+                                "MM/dd/yyyy hh:mm:ss a")).withColumn("data_as_of", # noqa
+                                to_timestamp(col("data_as_of"), "MM/dd/yyyy hh:mm:ss a") # noqa
+                                ).withColumn("data_loaded_at", to_timestamp(col("data_loaded_at"), # noqa
+                                "MM/dd/yyyy hh:mm:ss a")) .withColumn("requested_datetime", # noqa
+                                to_timestamp(col("requested_datetime"),  # noqa
+                                "MM/dd/yyyy hh:mm:ss a")) .withColumn("updated_datetime", # noqa
+                                to_timestamp(col("updated_datetime"),  # noqa
+                                "MM/dd/yyyy hh:mm:ss a")) .withColumn("lat",  # noqa
+                                col("lat").cast("double")) .withColumn("long",  # noqa
+                                col("long").cast("double"))) # noqa
 # TODO : output the transformed data to an s3 bucket
 
     return df_filtered.printSchema()
 
+
 if "__name__" == "__main__":
-    spark_transform(data).write.parquet("s3://final-aws-project/transformed/311-calls-data.parquet")
+    spark_transform(data).write.parquet(
+        "s3://final-aws-project/transformed/311-calls-data.parquet")
